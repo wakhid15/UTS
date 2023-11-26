e6cddadce0a286c28bbf4633f3e428a2113a058e
Mokhamad Wakhid Mauludien <95697443+wakhid15@users.noreply.github.com>
2023-11-26 21:13:00 +0700

    Delete uts_webframe-main directory

--- uts_webframe-main/resources/views/layouts/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer class="bg-primary text-white text-center pb-5">
-    <p>Created with <i class="bi bi-heart-fill text-danger"></i> by <a href="https://www.instagram.com/fillah_alfatih/" target="https://www.instagram.com/fillah_alfatih/">Irfan Adi Prastyo</a></p>
-</footer>
